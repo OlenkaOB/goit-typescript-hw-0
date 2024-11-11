41d892cc6682c6affc66ca595d5303399f5f342d
Borodkina Olena <elena.kaminnik@gmail.com>
2024-11-11 23:08:37 +0200

    inst

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <ErrorMessage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,7 @@
+import ErrorMessage from './ErrorMessage.module.css';
+
+const ErrorMessage = () => {
+  return <></>;
+};
+
+export default ErrorMessage;
