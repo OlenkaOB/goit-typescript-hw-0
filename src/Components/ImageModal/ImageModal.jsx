098fac89986da515958948887cfa69d09bcc647b
Borodkina Olena <elena.kaminnik@gmail.com>
2024-11-15 22:02:22 +0200

    modal

--- src/Components/ImageModal/ImageModal.jsx
+++ src/Components/ImageModal/ImageModal.jsx
@@ -1,5 +1,4 @@
 import s from './ImageModal.module.css';
-// import { IoIosClose } from 'react-icons/io';
 import Modal from 'react-modal';
 
 const ImageModal = ({ image, onClose }) => {
@@ -26,9 +25,6 @@ const ImageModal = ({ image, onClose }) => {
         <p>{alt_description || 'No description'}</p>
         <p>Author: {name}</p>
         <p>Likes: {likes} </p>
-        {/* <button className={s.closeBtn} onClick={onClose}>
-          <IoIosClose />
-        </button> */}
       </div>
     </Modal>
   );
