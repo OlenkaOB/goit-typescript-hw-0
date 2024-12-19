0e7219331f0a3df604b32213f0f5b47543070823
Borodkina Olena <elena.kaminnik@gmail.com>
2024-12-19 15:47:36 +0200

    i

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,4 @@
+import { React } from 'react';
 import { useEffect, useState } from 'react';
 import { fetchImages } from '../../services/Api';
 import ImageGallery from '../ImageGallery/ImageGallery';
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
+import { React } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './Components/App/App';
+import App from './components/App/App';
 import { Toaster } from 'react-hot-toast';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
     <Toaster position="bottom-right" reverseOrder={false} />
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
